bafb0ff4cf2255c16bd44f2046421640aaa3019a
adelino-dev <adelino.developer@gmail.com>
2023-01-01 23:05:52 -0300

    feat: add function text to binary

--- converter.py
+++ converter.py
@@ -4,3 +4,14 @@ def text_to_dec(text):
         decimal =  str(ord(letter))
         converted += decimal+ " "
     return converted
+
+def text_to_bin(text):
+    converted = ''
+    for letter in text:
+        binary =  bin(ord(letter))[2:]
+        length = len(binary)
+        if  length < 8:
+            binary = '0'*(8-length)+binary
+
+        converted += binary+" "    
+    return converted
